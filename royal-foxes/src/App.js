1272dc8993c22612f9f4b24a8892ce1698efeb61
Nathan Nguyen <nhan13574@gmail.com>
2020-07-07 16:01:27 -0500

    update

--- royal-foxes/src/App.js
+++ royal-foxes/src/App.js
@@ -1,36 +1,20 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Fox from './Components/Fox'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Fox from "./Components/Fox";
 
 function App() {
   return (
-        <div className="container">
-          <div className="jumbotron">
-            <h1>Hello world</h1>
-          </div>
-          <div className="row">
-            <Fox
-              name="Mr. Fox"
-              title="Knight"
-              furColor="red"
-            />
-            <Fox
-              name="Mr. Nhan"
-              title="Queen"
-              furColor="green"
-            />
-            <Fox
-              name="Mr. Nick"
-              title="Yikes"
-              furColor="blue"
-            />
-            <Fox
-              name="Mr. Dav"
-              title="Lol"
-              furColor="purple"
-              />
-          </div>
-        </div>
+    <div className="container">
+      <div className="jumbotron">
+        <h1>Hello world</h1>
+      </div>
+      <div className="row">
+        <Fox name="Mr. Fox" rank={5} furColor="red" />
+        <Fox name="Mr. Nhan" rank={3} furColor="green" />
+        <Fox name="Mr. Nick" rank={1} furColor="blue" />
+        <Fox name="Mr. Dav" rank={6} furColor="purple" />
+      </div>
+    </div>
   );
 }
 
--- royal-foxes/src/Components/Fox.jsx
+++ royal-foxes/src/Components/Fox.jsx
@@ -1,25 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+
+// eslint-disable-next-line
+const ranks = [
+  "Serf",
+  "Yeoman",
+  "Knights",
+  "Barons",
+  "Earl",
+  "Count",
+  "Duke",
+  "Emperor",
+];
 
 class Fox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentRank: this.props.rank,
+    };
+  }
+
+  levelUp = () => {
+    if (this.state.currentRank === ranks.length - 1) {
+      return;
+    }
+    this.setState({ currentRank: this.state.currentRank + 1 });
+  };
+
   render() {
-      return (
-        <div className="card col-sm-6">
-          <div className="card-body">
-            <h2 className="card-title">
-              {this.props.name}
-            </h2>
-              <p className="card-text">
-                Royal Title: { this.props.title }
-              </p>
-              <p className="card-text">
-                Fur Color: {this.props.furColor}
-            </p>
-            <button className="btn btn-primary">
-              Rank up
-            </button>
-          </div>
+    return (
+      <div className="card col-sm-6">
+        <div className="card-body">
+          <h2 className="card-title">{this.props.name}</h2>
+          <p className="card-text">
+            Royal Title: {ranks[this.state.currentRank]}
+          </p>
+          <p className="card-text">Fur Color: {this.props.furColor}</p>
+          <button className="btn btn-primary" onClick={this.levelUp}>
+            Rank up
+          </button>
         </div>
-      )
+      </div>
+    );
   }
 }
 
