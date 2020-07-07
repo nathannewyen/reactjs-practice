9b9aacc38d6d6e4a0c8508b860b229503b5fc1e8
Nathan Nguyen <nhan13574@gmail.com>
2020-07-07 16:56:52 -0500

    update

--- prop_it_up/src/Components/User.jsx
+++ prop_it_up/src/Components/User.jsx
@@ -1,15 +1,35 @@
 import React, { Component } from "react";
 
+const cardStyle = {
+  padding: "20px",
+  display: "inline-block",
+  margin: "10px",
+};
+
 class User extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentAge: this.props.age,
+    };
+  }
+
+  happyBirthday = () => {
+    this.setState({ currentAge: this.state.currentAge + 1 });
+  };
+
   render() {
     return (
-      <div className="card col-sm-6">
+      <div className="card col-sm-4" style={cardStyle}>
         <div className="card-body">
           <h2 className="card-title">
             {this.props.first_name}, {this.props.last_name}
           </h2>
-          <p className="card-text">Age: {this.props.age}</p>
+          <p className="card-text">Age: {this.state.currentAge}</p>
           <p className="card-text">Hair Color: {this.props.hairColor}</p>
+          <button className="btn btn-primary" onClick={this.happyBirthday}>
+            Happy Birthday
+          </button>
         </div>
       </div>
     );
