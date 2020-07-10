b6aaacfd38db611f1d426a4d84939f1d215a93d3
Nathan Nguyen <nhan13574@gmail.com>
2020-07-10 14:21:00 -0500

    tabs assignment

--- tabs/src/App.jsx
+++ tabs/src/App.jsx
@@ -1,13 +1,41 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import GroupButton from './components/Tabs';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import GroupButton from "./components/Tabs";
 
 function App() {
-	return (
-		<div className="App">
-			<GroupButton list={arr} handleButton={switchTextbox} />
-		</div>
-	);
+  const [tab1, setTab1] = useState(true);
+  const [tab2, setTab2] = useState(true);
+  const [tab3, setTab3] = useState(true);
+  const [text1, setText1] = useState("");
+  const [text2, setText2] = useState("");
+  const [text3, setText3] = useState("");
+  const switchTextbox = (e) => {
+    // let temp = (e.target.hidden) ? false : true
+    // alert (`${e.target.innerHTML}, Hidden: ${e.target.hidden}, Switch to: ${temp} `)
+    if (e.target.innerHTML === "Tab1") {
+      setTab1(false);
+      setTab2(true);
+      setTab3(true);
+    } else if (e.target.innerHTML === "Tab2") {
+      setTab1(true);
+      setTab2(false);
+      setTab3(true);
+    } else if (e.target.innerHTML === "Tab3") {
+      setTab1(true);
+      setTab2(true);
+      setTab3(false);
+    }
+  };
+  const arr = [
+    { status: tab1, show: setTab1, details: text1, change: setText1 },
+    { status: tab2, show: setTab2, details: text2, change: setText2 },
+    { status: tab3, show: setTab3, details: text3, change: setText3 },
+  ];
+  return (
+    <div className="App">
+      <GroupButton list={arr} handleButton={switchTextbox} />
+    </div>
+  );
 }
 
 export default App;
--- tabs/src/components/Tabs.jsx
+++ tabs/src/components/Tabs.jsx
@@ -1,29 +1,38 @@
-import react from 'react';
+import React from "react";
+
+const textareaStyle = {
+  marginTop: "30px",
+};
 
 const GroupButton = (props) => {
-	let list = props.list;
-	return (
-		<div className="jumbotron">
-			<div class="container btn-group" role="group">
-				{list.map((button, i) => (
-					<button key={i} className="btn btn-dark" onClick={(e) => props.handleButton(e)}>
-						Tab{i + 1}
-					</button>
-				))}
-			</div>
-			<div className="container d-flex">
-				{list.map((text, i) => (
-					<textarea
-						key={i}
-						cols="100"
-						row={6}
-						hidden={text.status}
-						value={text.details}
-						onChange={(e) => text.change(e.target.value)}
-					/>
-				))}
-			</div>
-		</div>
-	);
+  let list = props.list;
+  return (
+    <div className="container-fluid">
+      <div class="container btn-group" role="group" aria-label="Basic example">
+        {list.map((button, i) => (
+          <button
+            key={i}
+            className="btn btn-dark"
+            onClick={(e) => props.handleButton(e)}
+          >
+            Tab{i + 1}
+          </button>
+        ))}
+      </div>
+      <div>
+        {list.map((text, i) => (
+          <textarea
+            style={textareaStyle}
+            key={i}
+            cols="140"
+            row={6}
+            hidden={text.status}
+            value={text.details}
+            onChange={(e) => text.change(e.target.value)}
+          ></textarea>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default GroupButton;
